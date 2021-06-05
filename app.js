16484ffb52d13a5909c9d69648ab211a82aca84c
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-05 13:26:02 -0700

    created basic get and post on root route

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const app = express();
 
 app.use(logger('dev'));
 
+app.use(express.json());
+
 app
   .route('/')
   .get((request, response) => response.send('This is my app yall!'))
   .post((req, res) => {
-    console.log('Hitting our post');
+    console.log(req.body);
+    res.json({ message: 'message received, homie' });
   });
 
 app.listen(3000, () => console.log('Running on port 3000'));
