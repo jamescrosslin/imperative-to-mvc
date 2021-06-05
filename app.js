8f86164be1bbc076b73107b04b1b472d76e7c6ea
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-05 13:51:08 -0700

    added ejs and serving simple views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const ejs = require('ejs');
 
 const app = express();
 
@@ -7,12 +8,19 @@ app.use(logger('dev'));
 
 app.use(express.json());
 
-app
-  .route('/')
-  .get((request, response) => response.send('This is my app yall!'))
-  .post((req, res) => {
-    console.log(req.body);
-    res.json({ message: 'message received, homie' });
-  });
+app.set('view engine', 'ejs');
+
+app.route('/').get((request, response) => response.render('../feed', { title: 'Root route' }));
+
+app.get('/posts', (req, res) =>
+  res.render('../posts.ejs', {
+    posts: ['This is the post I want', 'This is also a post that I want'],
+  }),
+);
+
+app.route('/api').post((req, res) => {
+  console.log(req.body);
+  res.json({ message: 'message received, homie' });
+});
 
 app.listen(3000, () => console.log('Running on port 3000'));
